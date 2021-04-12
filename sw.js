ccbf8f2517fc037d44bee72888263e2d423d6631
stefan.kohlbacher <bb9a3f4ecc360c7911919fb4b5414cf239a01735>
2021-04-12 17:36:07 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c5d5d6d8370c62228c2f.js"
+    "url": "webpack-runtime-c1b0636fcea2524e830d.js"
   },
   {
     "url": "styles.9e13fea04b1a3e55a053.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b00b582a980c7d90d603a0ed13bf6be0"
+    "revision": "5b936b4b0a6be81f578c57d231a2c062"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a95b272f8c25b9a8a758.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "5df531d3cdef4341c7767b6fb0bc2853"
+    "revision": "88e5ac4ff54815cafccec8c7781e7f15"
   },
   {
     "url": "polyfill-9524f89a0a3261402e06.js"
